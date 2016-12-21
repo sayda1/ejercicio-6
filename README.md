e53afa70c981bc475c29b57da6c5b41877ace62c
sayda1 <saydaquintanilla1@gmail.com>
2016-12-21 17:33:16 -0500

    html y pseudocodigos

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # ejercicio-6
+Incio:Parametro
+
+## Definir Variables
+
+- weekday
+- vacations
+- puede-dormir
+
+## Proceso
+si vacationes es verdadero
+  dormimos
+sino
+	no dormimos
+si  weekday es verdadero
+	no dormimos
+si no
+	dormimos
+
+##fin
---
+++ main.js
@@ -0,0 +1,26 @@
+function Calcular() {
+    var vacation = document.getElementById("vacation").value;
+	var weekeday = document.getElementById("dia").value;
+	var isVacation = true;
+	var weekeday = false;
+	var salida = document.getElementById("salida");
+	
+	if(vacation == "2"){
+        isVacation =false;
+    }
+    else
+    {
+        vacation =true;
+        
+    }
+    
+    if(weekeday="1"){
+        isweekeda =true;
+    }
+    
+    else{
+        weekeday=false;
+    }
+  
+}
+
